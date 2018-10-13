de12f82e7222d67c5161e57e2caf6d43baaa78b1
godlike375 <32139587+godlike375@users.noreply.github.com>
2018-10-13 15:14:48 +0300

    upd

--- MyClass.cs
+++ MyClass.cs
@@ -1,12 +1,4 @@
-﻿/*
- * Created by SharpDevelop.
- * User: godlike
- * Date: 08.10.2018
- * Time: 21:56
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Collections;
 
