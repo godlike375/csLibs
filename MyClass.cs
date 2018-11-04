44aba3ed7000ab363ad4e7fab446937027699519
godlike375 <persikabricos@gmail.com>
2018-11-05 00:49:18 +0300

    added neural net classes

--- MyClass.cs
+++ MyClass.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
 using System.Collections;
 
 namespace extensions
@@ -35,6 +37,115 @@ namespace extensions
 	        }
 	        return new string(res);
 	    }
+	}
+	
+	public class Neuron
+    {
+    	public static float Sigmoid(float res)
+ 		{
+ 			if(res > 1)
+ 				return 1.0f;
+ 			if(res < 0)
+ 				return 0;
+ 			return res;
+ 		}
+    	public delegate float active(float res);
+    	//тормозящие связи = отрицательные веса
+    	public float power;
+    	public List<float> w = new List<float>();
+    	public List<Neuron> d = new List<Neuron>();
+    	public List<Tuple<Neuron, int>> a = new List<Tuple<Neuron, int>>();
+    	public Neuron(int ka=1, int kd=1)
+    	{
+    		a = new List<Tuple<Neuron, int>>(ka);
+    		w = new List<float>(kd);
+    	}
+    	public void Connect(Neuron to, float stW=1.0f)
+    	{
+    		a.Add(new Tuple<Neuron, int>(to, to.w.Count));
+    		to.w.Add(stW);
+    		to.d.Add(this);
+    	}
+    	
+    	public void Disconnect(int acs)
+    	{
+    		a[acs].Item1.w.RemoveAt(a[acs].Item2);
+    		a.RemoveAt(acs);
+    	}
+    	
+    	public void Push(Func<float, float> ac)
+    	{
+    		if(ac(power)>0)       			
+    		foreach(var n in a)
+    			n.Item1.power+=n.Item1.w[n.Item2]*power;
+    	}
+    }
+	
+	public class NeuralNet
+	{
+		public List<List<Neuron>> layers = new List<List<Neuron>>();
+		public void AddLayer(int cnt=1)
+		{
+			layers.Add(new List<Neuron>(cnt));
+			for(int i=0; i<cnt; i++)
+				layers[layers.Count-1].Add(new Neuron(1, (layers.Count-2) > 0 ? layers[layers.Count-1].Count:1));
+		}
+		public static List<Neuron> ParallelCreate(int thCnt, int neurCnt)
+		{
+			int cnt = neurCnt/thCnt;
+			var lists = new List<List<Neuron>>(thCnt);
+			var capL = new List<int>();
+			for(int i=0; i<thCnt; i++)
+			{
+				if(neurCnt%thCnt>0 && i==thCnt-1)
+				{
+					lists.Add(new List<Neuron>(cnt+neurCnt%thCnt));
+					capL.Add(cnt+neurCnt%thCnt);
+				}
+				else
+				{
+					lists.Add( new List<Neuron>(cnt));
+					capL.Add(cnt);
+				}
+			}
+			var t = new Task[thCnt];
+			var s = new Stack<int>();
+			for(int i=0; i<thCnt; i++)
+				s.Push(i);
+			var acc=0;
+			foreach(var ss in capL)
+				acc+=ss;
+			if(acc!=neurCnt)
+				Console.WriteLine("lol "+acc.ToString());
+			for(int i=0; i<thCnt; i++)
+			{
+				t[i]=Task.Factory.StartNew(()=>
+        	{
+        		int k = s.Pop();
+        		for(int j=0; j<capL[k]; j++)
+				lists[k].Add(new Neuron(1,1));
+        	});	
+			}
+			Task.WaitAll(t);
+			
+			var res = new List<Neuron>(neurCnt);
+			for(int i=0; i<thCnt; i++)
+				res.AddRange(lists[i]);
+			return res;
+		}
+		
+		public void Iterate()
+		{
+			foreach(var l in layers)
+				ParallelPush(l, Neuron.Sigmoid);
+		}
+
+		public static void ParallelPush(List<Neuron> ne, Func<float, float> ac)
+		{
+			foreach(var n in ne)
+				n.Push(ac);
+			//Parallel.ForEach(ne, delegate(Neuron n){n.Push(ac);});
+		}
 	}
 	
 	public class pair<O,T>
